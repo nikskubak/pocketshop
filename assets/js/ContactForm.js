d25e90340ffc6b3775f5d64a41ecab8ec08de75d
Nikita Skubak <nikskubak@gmail.com>
2018-06-28 00:16:26 +0300

    chane content

--- assets/js/ContactForm.js
+++ assets/js/ContactForm.js
@@ -1,25 +1,3 @@
-// function submit($e) {
-//   var $form = $('form#test-form'),
-//       // url = 'https://script.google.com/macros/s/AKfycbxZDgMbC6g85j8nwT5B5WShz1-SWkMJC0aTNDoR3RbnNUHdT7M/exec'
-//         url = 'http://api.premiumsale.com.ua'
-//   $e.preventDefault()
-//   var jqxhr = $.ajax({
-//     url: url,
-//     method: "GET",
-//     dataType: "json",
-//     data: $form.serializeObject()
-//   }).done(
-//     function(response){
-//       console.log(response);
-//     }
-//   ).fail(
-//     function(response){
-//       console.log(response);
-//     }
-//   );
-//   return false;
-// }
-
 var $form = $('form#test-form'),
     url = 'https://script.google.com/macros/s/AKfycbxZDgMbC6g85j8nwT5B5WShz1-SWkMJC0aTNDoR3RbnNUHdT7M/exec'
 
@@ -27,7 +5,7 @@ $('#submit-form').on('click', function(e) {
   e.preventDefault();
   var jqxhr = $.ajax({
     url: url,
-    method: "GET",
+    method: "POST",
     dataType: "json",
     data: $form.serializeObject()
   }).success(
